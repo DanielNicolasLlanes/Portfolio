e5e0416db9fc5d1eeafa6662ec92940f0b4faba5
Daniel <daniel.nicolas.llanes@gmail.com>
2026-04-26 00:02:18 -0300

    primer comit

---
+++ src/Button-primary/Button-primary.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Button-primary.css'
+
+const ButtonPrimary = () => {
+  return (
+     <button className='button-primary'>Explorar</button>
+  )
+}
+
+export default ButtonPrimary
---
+++ src/Card/Card.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './Card.css'
+import '../Paragraph/Paragraph.css'
+import Paragraph from '../Paragraph/Paragraph'
+import Image from '../Image/Image'
+
+const Card = () => {
+  return (
+    <div className="glass-card">
+      <div className="card-header">
+        <Image/>
+        <div className="header-text">
+          <h1 className='title'>Daniel Llanes</h1>
+          <p className='text'>Full Stack Developer</p>
+        </div>
+      </div>
+      <hr className='line'/>  
+      <Paragraph/>
+    </div>
+  )
+}
+
+export default Card
